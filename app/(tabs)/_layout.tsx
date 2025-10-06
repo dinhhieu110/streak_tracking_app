ab10cc12c6d63ab722b5b866ba104125c6e35ac9
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-10-07 00:43:42 +0700

    feat: Build common tabs for mobile navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,22 @@
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Home",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <FontAwesome5 name="home" size={24} color={color} />
+            ) : (
+              <Ionicons name="home-outline" size={24} color="black" />
+            ),
+        }}
+      />
+      <Tabs.Screen name="login" options={{ headerTitle: "Login" }} />
+    </Tabs>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "Home" }} />
-      <Stack.Screen name="login" options={{ headerTitle: "Login" }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
